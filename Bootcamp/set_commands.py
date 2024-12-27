888a8bd9ef019c09b5ede7b785e8c350416b5e5e
Leetisdeep <aaaooogjg@gmail.com>
2024-12-27 09:52:05 +0300

    Update set_commands.py

--- Bootcamp/set_commands.py
+++ Bootcamp/set_commands.py
@@ -2,8 +2,10 @@ from aiogram.types import BotCommand
 
 async def set_default_commands(bot):
     bot_commands = ([
-        BotCommand(command='/discounts',description='Скидки !'),
+        BotCommand(command='/discounts',description='Скидки!'),
         BotCommand(command='/ping',description='Проверка на скорость работы бота.'),
+        BotCommand(command='/contact',description='Возможность написать пожелания или предложения создателям данного бота.'),
+        BotCommand(command='/my_desired',description='Возможность следить за конкретными играми.'),
     ])
 
     await bot.set_my_commands(bot_commands)
